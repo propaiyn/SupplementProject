ef8e2954d23722eed92ed8de2cc538c10df65842
Hassan <hasxanshahaab@gmail.com>
2022-08-19 12:30:32 +0100

    Spring Initialzr Starter

---
+++ src/main/java/com/qa/SupplementProject/SupplementProjectApplication.java
@@ -0,0 +1,13 @@
+package com.qa.SupplementProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplementProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplementProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/SupplementProject/SupplementProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.SupplementProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplementProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
